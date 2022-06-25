45e344a9a9fd7fd7127d8c70643eed65ae7a7c1d
Nakadie <77523003+Nakadie@users.noreply.github.com>
2022-06-25 17:36:00 +0900

    solved num to string puzzle

---
+++ src/main/scala/codewars/numToStr.scala
@@ -0,0 +1,11 @@
+package codewars
+
+object numToStr {
+  def main(args: Array[String]): Unit = {
+    def numberToString(n: Int): String = {
+      n.toString
+  }
+    println(numberToString(1234))
+  }
+
+}
