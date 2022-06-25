3c75af03c2215efaf480452230b2c96ca5b500db
Nakadie <77523003+Nakadie@users.noreply.github.com>
2022-06-25 13:08:41 +0900

    solved pillars puzzle

---
+++ src/main/scala/codewars/Pillars.scala
@@ -0,0 +1,19 @@
+package codewars
+
+object Pillars {
+  def main(args: Array[String]): Unit = {
+    def distance(num_pill: Int, dist: Int, width: Int): Int = {
+      val answer = if (num_pill == 1) {
+        0
+      } else if (num_pill == 2) {
+        dist * 100
+      } else {
+        ((num_pill - 1) * (dist * 100)) + (num_pill * width) - (width * 2)
+      }
+      answer
+    }
+    println(distance(1, 111, 25))
+    println(distance(2,20,25)) // 2000
+    println(distance(11, 15, 30)) // 15270
+  }
+}
